1a1b5cb24e44a795c16a3fc9d2e0c178de2ac78f
Chick <chetwynd.chick@gmail.com>
2023-04-10 15:55:52 -0700

    Added to the script

--- custom_functions.py
+++ custom_functions.py
@@ -53,3 +53,32 @@ def mean(data_set, index):
 
 avg_price = mean(apps_data, 4)
 print(avg_price)
+
+
+
+# This script below creates a function that cleans all of the strings in the list and
+# removes unwanted characters as defined in a separate list.
+
+test_data = ["1912", "1929", "1913-1923",
+             "(1951)", "1994", "1934",
+             "c. 1915", "1995", "c. 1912",
+             "(1988)", "2002", "1957-1959",
+             "c. 1955.", "c. 1970's",
+             "C. 1990-1999"]
+
+bad_chars = ["(", ")", "c", "C", ".", "s", "'", " "]
+
+
+def strip_characters(string):
+    for char in bad_chars:
+        string = string.replace(char, "")
+    return string
+
+
+stripped_test_data = []
+
+for element in test_data:
+    element = strip_characters(element)
+    stripped_test_data.append(element)
+
+print(stripped_test_data)
